20b282d43a7401fe57f4db6671a625c4b86fe45d
logic <128760842+white-base@users.noreply.github.com>
2024-09-05 17:20:36 +0900

    app.js 업데이트

--- notice/vue_mix/app.js
+++ notice/vue_mix/app.js
@@ -3,10 +3,8 @@ import NoticeForm from './components/NoticeForm.js';
 import NoticeAdminService from './service/notice-admin-svc.js'
 
 const { createApp, ref } = Vue
-
-// Vue.devtools = true; // e
-
 const bm = new _L.BindModel(new NoticeAdminService());  
+
 bm.url =' /notice/data/list.json';
 
 const app = createApp({
@@ -23,13 +21,6 @@ const app = createApp({
     };
   },
   methods: {
-    // fetchNotices() {
-    //   fetch('/notice/data/list.json')
-    //     .then(response => response.json())
-    //     .then(data => {
-    //       this.notices = data.rows || [];
-    //     });
-    // },
     async selectNotice(idx) {
       bm.cmd['read'].outputOption.index = Number(idx);
       await bm.command['read'].execute();
@@ -38,20 +29,6 @@ const app = createApp({
     deselectNotice() {
       this.selectedNotice = null;
     },
-    // updateNotice(notice) {
-    //   // POINT: 여기서 처리하면 됨
-    //   // bm.
-    //   alert('Notice updated successfully');
-    //   this.deselectNotice();
-    // },
-    // deleteNotice() {
-    //   alert('Notice deleted successfully');
-    //   this.deselectNotice();
-    // }
-  },
-  async mounted() {
-    // await bm.cmd['list'].execute();
-    // this.fetchNotices();
   },
   components: {
     'notice-list': NoticeList,
